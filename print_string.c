b67ca5c03275dc4609f71df31eeaed6f85c1b142
Hussam <samdevy@outlook.com>
2025-10-02 13:17:32 +0300

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -4,13 +4,13 @@ int print_string(char *str)
 {
 int count = 0;
 
-    if (!str)
-        str = "(null)";
+if (!str)
+str = "(null)";
 
-    while (*str)
-    {
-        count += _putchar(*str);
-        str++;
-    }
-    return (count);
+while (*str)
+{
+count += _putchar(*str);
+str++;
+}
+return (count);
 }
