67e9625a6498837137146f2407a30990ddd0edca
Hussam <samdevy@outlook.com>
2025-10-02 13:21:12 +0300

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -1,16 +1,22 @@
 #include "main.h"
 
+/**
+ * print_string - Prints a string to stdout, character by character.
+ * @str: Pointer to the string to print. If NULL, prints "(null)".
+ *
+ * Return: Always returns the number of characters printed (success).
+ */
 int print_string(char *str)
 {
-int count = 0;
+	int count = 0;
 
-if (!str)
-str = "(null)";
+	if (!str)
+		str = "(null)";
 
-while (*str)
-{
-count += _putchar(*str);
-str++;
-}
-return (count);
+	while (*str)
+	{
+		count += _putchar(*str);
+		str++;
+	}
+	return (count);
 }
