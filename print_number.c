5977555b8af13d723cc363e8b13a74437dbffa53
Hussam <samdevy@outlook.com>
2025-10-02 13:23:40 +0300

    Update print_number.c

--- print_number.c
+++ print_number.c
@@ -1,5 +1,13 @@
 #include "main.h"
+
+/**
+ * print_number - Prints an integer to stdout.
+ * @n: The integer to print (currently unused).
+ *
+ * Return: Always returns 0 (success).
+ */
 int print_number(int n)
-{(void)n;
-return (0);
+{
+	(void)n;
+	return (0);
 }
