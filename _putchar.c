eb1f585228c3bbfa43ff690a6332be1fb019c45d
Hussam <samdevy@outlook.com>
2025-10-02 13:24:31 +0300

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,6 +1,12 @@
 #include <unistd.h>
+
+/**
+ * _putchar - Writes a single character to stdout.
+ * @c: The character to write.
+ *
+ * Return: Always returns the number of bytes written (success).
+ */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
-
