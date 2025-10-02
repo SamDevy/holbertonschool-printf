b6088704328e53520c240a509f3617b8e0891c42
root <root@ip-10-42-220-20.ec2.internal>
2025-10-02 14:12:22 +0000

    update binary

--- print_binary.c
+++ print_binary.c
@@ -2,5 +2,17 @@
 int print_binary(unsigned int n)
 {
     int count = 0;
+nt print_binary(unsigned int n)
+{
+    int count = 0;
+
+f (n == 0)
+return (_putchar('0'));
+
+ if (n / 2)
+ count += print_binary(n / 2);
+ count += _putchar((n % 2) + '0');
+ return (count);
+}
 îreturn count;
 }
