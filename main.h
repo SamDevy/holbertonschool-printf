968bea702f3b3d21191c3e88c59d3f8819b25038
Hussam <samdevy@outlook.com>
2025-10-05 00:07:37 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,3 +1,4 @@
+/* main.h */
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -5,17 +6,15 @@
 
 int _printf(const char *format, ...);
 
-/* Existing helpers you already have */
 int _putchar(char c);
 int print_string(char *s);
 int print_number(int n);
 int print_binary(unsigned int n);
 
-/* New unsigned conversion prototypes */
 int print_unsigned_rec(unsigned int n, const char *digits, int base);
-int print_unsigned(unsigned int n);      /* %u */
-int print_octal(unsigned int n);         /* %o */
-int print_hex_lower(unsigned int n);     /* %x */
-int print_hex_upper(unsigned int n);     /* %X */
+int print_unsigned(unsigned int n);
+int print_octal(unsigned int n);
+int print_hex_lower(unsigned int n);
+int print_hex_upper(unsigned int n);
 
 #endif /* MAIN_H */
